74d16221f29af596ae68257f60567ff1f35e4a3a
Carlos Ojeda <cp665005@gmail.com>
2024-03-05 04:22:17 -0600

    "Comienzo y Comentarios del Proyecto (Integrantes y Tareas del Equipo)"

---
+++ src/CorreoEletronico.java
@@ -0,0 +1,35 @@
+
+public class CorreoEletronico {
+    
+	//Comienzo y Comentarios del proyecto (Correo Electronico con cola de mensajes):
+	//1 //Integrantes del equipo:
+	//1.1 // Caamal Franco, Yazmin Guadalupe. RAMA: RAMA_YAZMIN.
+	//1.2 // Diaz Jimenez, Nallely Patricia. RAMA: RAMA_NALLELY.
+	//1.3 // Ojeda Pereira, Carlos Abraham. RAMA: RAMA_CARLOS.
+	
+	//2// Tareas del equipo:
+	//2.1 //Ahora, se hará el cambio de la master a la rama del lider Carlos Abraham Ojeda Pereira (RAMA_CARLOS) para su edición.
+	//2.2 //Asimismo, Carlos Abraham Ojeda Pereira es el responsable de integrar todas las ramas a la rama principal y hacer su parte asignada del proyecto en su respectiva rama.
+	//2.3 // Caamal Franco, Yazmin Guadalupe y Diaz Jimenez, Nallely Patricia harán sus partes asignadas del proyecto en sus respectivas ramas.
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
