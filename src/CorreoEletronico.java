684ff0702a4f4df258b835e80e36dd9f360e7130
Carlos Ojeda <cp665005@gmail.com>
2024-03-05 05:12:28 -0600

    "Creación de la clase CorreoElectronico (con sus Atributos, Constructor, Get y Set, y, Comparación con el atributo tipoDeMensaje para ordenar La Cola de la esta misma clase)"

--- src/CorreoEletronico.java
+++ src/CorreoEletronico.java
@@ -1,5 +1,5 @@
 
-public class CorreoEletronico {
+public class CorreoEletronico implements Comparable<CorreoEletronico>{
     
 	//Comienzo y Comentarios del proyecto (Correo Electronico con cola de mensajes):
 	//1 //Integrantes del equipo:
@@ -8,28 +8,74 @@ public class CorreoEletronico {
 	//1.3 // Ojeda Pereira, Carlos Abraham. RAMA: RAMA_CARLOS.
 	
 	//2// Tareas del equipo:
-	//2.1 //Ahora, se hará el cambio de la master a la rama del lider Carlos Abraham Ojeda Pereira (RAMA_CARLOS) para su edición.
+	//2.1 //Ahora, se hara el cambio de la master a la rama del lider Carlos Abraham Ojeda Pereira (RAMA_CARLOS) para su edicion.
 	//2.2 //Asimismo, Carlos Abraham Ojeda Pereira es el responsable de integrar todas las ramas a la rama principal y hacer su parte asignada del proyecto en su respectiva rama.
-	//2.3 // Caamal Franco, Yazmin Guadalupe y Diaz Jimenez, Nallely Patricia harán sus partes asignadas del proyecto en sus respectivas ramas.
+	//2.3 // Caamal Franco, Yazmin Guadalupe y Diaz Jimenez, Nallely Patricia haran sus partes asignadas del proyecto en sus respectivas ramas.
 	
+	//Atributos de la clase:
+	private String emisor; 
+	private String receptor;
+	private String asunto;
+	private String mensaje;
+	private int tipoDeMensaje; // 1 personal, 2 trabajo, 3 otro tipo.
 	
+	//Constructor de la clase:
+	public CorreoEletronico (String emisor, String receptor, String asunto, String mensaje, int tipoDeMensaje) {
+		
+		this.emisor = emisor;
+		this.receptor = receptor;
+		this.asunto = asunto;
+		this.mensaje = mensaje;
+		this.tipoDeMensaje = tipoDeMensaje;
+	}
+    
+    //Get y Set de la clase CorreoElectronico:
+	public String getEmisor() {
+		return emisor;
+	}
+
+	public void setEmisor(String emisor) {
+		this.emisor = emisor;
+	}
+
+	public String getReceptor() {
+		return receptor;
+	}
+
+	public void setReceptor(String receptor) {
+		this.receptor = receptor;
+	}
+
+	public String getAsunto() {
+		return asunto;
+	}
+
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public int getTipoDeMensaje() {
+		return tipoDeMensaje;
+	}
+
+	public void setTipoDeMensaje(int tipoDeMensaje) {
+		this.tipoDeMensaje = tipoDeMensaje;
+	}
+    
+	//Comparacion entre correos electronicos con el atributo tipoDeMensaje para la cola de la clase CorreoElectronico:
+	@Override
+    public int compareTo(CorreoEletronico otroCorreoEletronico) {
+    
+	// Comparar personas basadas en su tipo:
+        return Integer.compare(this.tipoDeMensaje, otroCorreoEletronico.getTipoDeMensaje());
+    }
 	
 }
